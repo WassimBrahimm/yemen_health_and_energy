ab409d4bda740f5499f19c044a91caaaff2adba1
WassimBrahimm <mrwassimibrahim@gmail.com>
2022-11-30 16:00:45 +0100

    fixed for Adnan

--- clustering/tutorial/cities_clustering.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-raw_data_frame = pd.read_csv("http://factominer.free.fr/livreV2/temperat.csv")
--- clustering/tutorial/kjkb.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_url = 'https://comtrade.un.org/api/get?max=500&type=C&freq=A&px=HS&ps=2017&r=all&p=0&rg=all&cc=TOTAL&fmt=csv'
-response = requests.get(api_url)
-data = response.content
-
-with open('output.csv', 'wb') as output:
-    print(data)
